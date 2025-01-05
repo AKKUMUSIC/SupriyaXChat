b873b2fc910b838a9917ac793a4af7990ea359c6
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2025-01-05 04:49:46 +0000

    Update boadcast.py

--- Mickey/modules/boadcast.py
+++ Mickey/modules/boadcast.py
@@ -4,6 +4,7 @@ from pyrogram import filters, Client
 from pyrogram.types import Message
 
 from Mickey import OWNER, MickeyBot
+from pyrogram.errors import FloodWait, ChatAdminRequired
 from Mickey.database.chats import get_served_chats
 from Mickey.database.users import get_served_users
 
