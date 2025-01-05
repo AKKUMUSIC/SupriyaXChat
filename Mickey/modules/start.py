dba0dded41faeb204780163b29088e14829aec1f
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2025-01-05 03:05:12 +0000

    Update start.py

--- Mickey/modules/start.py
+++ Mickey/modules/start.py
@@ -99,149 +99,149 @@ async def welcome(_, m: Message):
 # broadcast_lock = asyncio.Lock()
 
 
-@MickeyBot.on_cmd("rrepo") & filters.user(OWNER))
-async def broadcast_message(client, message):
-    global IS_BROADCASTING
-    bot_id = (await client.get_me()).id
-    clone_id = (await client.get_me()).id
-    user_id = message.from_user.id
-    if not await is_owner(clone_id, user_id):
-        await message.reply_text("You don't have permission to use this command on this bot.")
-        return
+# @MickeyBot.on_cmd("rrepo") & filters.user(OWNER))
+# async def broadcast_message(client, message):
+#     global IS_BROADCASTING
+#     bot_id = (await client.get_me()).id
+#     clone_id = (await client.get_me()).id
+#     user_id = message.from_user.id
+#     if not await is_owner(clone_id, user_id):
+#         await message.reply_text("You don't have permission to use this command on this bot.")
+#         return
         
-    async with broadcast_lock:
-        if IS_BROADCASTING:
-            return await message.reply_text(
-                "A broadcast is already in progress. Please wait for it to complete."
-            )
+#     async with broadcast_lock:
+#         if IS_BROADCASTING:
+#             return await message.reply_text(
+#                 "A broadcast is already in progress. Please wait for it to complete."
+#             )
 
-        IS_BROADCASTING = True
-        try:
-            query = message.text.split(None, 1)[1].strip()
-        except IndexError:
-            query = message.text.strip()
-        except Exception as eff:
-            return await message.reply_text(
-                f"**Error**: {eff}"
-            )
-        try:
-            if message.reply_to_message:
-                broadcast_content = message.reply_to_message
-                broadcast_type = "reply"
-                flags = {
-                    "-pin": "-pin" in query,
-                    "-pinloud": "-pinloud" in query,
-                    "-nogroup": "-nogroup" in query,
-                    "-user": "-user" in query,
-                }
-            else:
-                if len(message.command) < 2:
-                    return await message.reply_text(
-                        "**Please provide text after the command or reply to a message for broadcasting.**"
-                    )
+#         IS_BROADCASTING = True
+#         try:
+#             query = message.text.split(None, 1)[1].strip()
+#         except IndexError:
+#             query = message.text.strip()
+#         except Exception as eff:
+#             return await message.reply_text(
+#                 f"**Error**: {eff}"
+#             )
+#         try:
+#             if message.reply_to_message:
+#                 broadcast_content = message.reply_to_message
+#                 broadcast_type = "reply"
+#                 flags = {
+#                     "-pin": "-pin" in query,
+#                     "-pinloud": "-pinloud" in query,
+#                     "-nogroup": "-nogroup" in query,
+#                     "-user": "-user" in query,
+#                 }
+#             else:
+#                 if len(message.command) < 2:
+#                     return await message.reply_text(
+#                         "**Please provide text after the command or reply to a message for broadcasting.**"
+#                     )
                 
-                flags = {
-                    "-pin": "-pin" in query,
-                    "-pinloud": "-pinloud" in query,
-                    "-nogroup": "-nogroup" in query,
-                    "-user": "-user" in query,
-                }
+#                 flags = {
+#                     "-pin": "-pin" in query,
+#                     "-pinloud": "-pinloud" in query,
+#                     "-nogroup": "-nogroup" in query,
+#                     "-user": "-user" in query,
+#                 }
 
-                for flag in flags:
-                    query = query.replace(flag, "").strip()
+#                 for flag in flags:
+#                     query = query.replace(flag, "").strip()
 
-                if not query:
-                    return await message.reply_text(
-                        "Please provide a valid text message or a flag: -pin, -nogroup, -pinloud, -user"
-                    )
+#                 if not query:
+#                     return await message.reply_text(
+#                         "Please provide a valid text message or a flag: -pin, -nogroup, -pinloud, -user"
+#                     )
 
                 
-                broadcast_content = query
-                broadcast_type = "text"
+#                 broadcast_content = query
+#                 broadcast_type = "text"
             
 
-            await message.reply_text("**Started broadcasting...**")
+#             await message.reply_text("**Started broadcasting...**")
 
-            if not flags.get("-nogroup", False):
-                sent = 0
-                pin_count = 0
-                async for dialog in client.get_dialogs():
-                    chat_id = dialog.chat.id
-                    if chat_id == message.chat.id:
-                        continue
-                    try:
-                        if broadcast_type == "reply":
-                            m = await client.forward_messages(
-                                chat_id, message.chat.id, [broadcast_content.id]
-                            )
-                        else:
-                            m = await client.send_message(
-                                chat_id, text=broadcast_content
-                            )
-                        sent += 1
-                        await asyncio.sleep(20)
+#             if not flags.get("-nogroup", False):
+#                 sent = 0
+#                 pin_count = 0
+#                 async for dialog in client.get_dialogs():
+#                     chat_id = dialog.chat.id
+#                     if chat_id == message.chat.id:
+#                         continue
+#                     try:
+#                         if broadcast_type == "reply":
+#                             m = await client.forward_messages(
+#                                 chat_id, message.chat.id, [broadcast_content.id]
+#                             )
+#                         else:
+#                             m = await client.send_message(
+#                                 chat_id, text=broadcast_content
+#                             )
+#                         sent += 1
+#                         await asyncio.sleep(20)
 
-                        if flags.get("-pin", False) or flags.get("-pinloud", False):
-                            try:
-                                await m.pin(
-                                    disable_notification=flags.get("-pin", False)
-                                )
-                                pin_count += 1
-                            except Exception as e:
-                                continue
+#                         if flags.get("-pin", False) or flags.get("-pinloud", False):
+#                             try:
+#                                 await m.pin(
+#                                     disable_notification=flags.get("-pin", False)
+#                                 )
+#                                 pin_count += 1
+#                             except Exception as e:
+#                                 continue
 
-                    except FloodWait as e:
-                        flood_time = int(e.value)
-                        logger.warning(
-                            f"FloodWait of {flood_time} seconds encountered for chat {chat_id}."
-                        )
-                        if flood_time > 200:
-                            logger.info(
-                                f"Skipping chat {chat_id} due to excessive FloodWait."
-                            )
-                            continue
-                        await asyncio.sleep(flood_time)
-                    except Exception as e:
+#                     except FloodWait as e:
+#                         flood_time = int(e.value)
+#                         logger.warning(
+#                             f"FloodWait of {flood_time} seconds encountered for chat {chat_id}."
+#                         )
+#                         if flood_time > 200:
+#                             logger.info(
+#                                 f"Skipping chat {chat_id} due to excessive FloodWait."
+#                             )
+#                             continue
+#                         await asyncio.sleep(flood_time)
+#                     except Exception as e:
                         
-                        continue
+#                         continue
 
-                await message.reply_text(
-                    f"**Broadcasted to {sent} chats and pinned in {pin_count} chats.**"
-                )
+#                 await message.reply_text(
+#                     f"**Broadcasted to {sent} chats and pinned in {pin_count} chats.**"
+#                 )
 
-            if flags.get("-user", False):
-                susr = 0
-                async for dialog in client.get_dialogs():
-                    chat_id = dialog.chat.id
-                    try:
-                        if broadcast_type == "reply":
-                            m = await client.forward_messages(
-                                user_id, message.chat.id, [broadcast_content.id]
-                            )
-                        else:
-                            m = await client.send_message(
-                                user_id, text=broadcast_content
-                            )
-                        susr += 1
-                        await asyncio.sleep(20)
+#             if flags.get("-user", False):
+#                 susr = 0
+#                 async for dialog in client.get_dialogs():
+#                     chat_id = dialog.chat.id
+#                     try:
+#                         if broadcast_type == "reply":
+#                             m = await client.forward_messages(
+#                                 user_id, message.chat.id, [broadcast_content.id]
+#                             )
+#                         else:
+#                             m = await client.send_message(
+#                                 user_id, text=broadcast_content
+#                             )
+#                         susr += 1
+#                         await asyncio.sleep(20)
 
-                    except FloodWait as e:
-                        flood_time = int(e.value)
-                        logger.warning(
-                            f"FloodWait of {flood_time} seconds encountered for user {user_id}."
-                        )
-                        if flood_time > 200:
-                            logger.info(
-                                f"Skipping user {user_id} due to excessive FloodWait."
-                            )
-                            continue
-                        await asyncio.sleep(flood_time)
-                    except Exception as e:
+#                     except FloodWait as e:
+#                         flood_time = int(e.value)
+#                         logger.warning(
+#                             f"FloodWait of {flood_time} seconds encountered for user {user_id}."
+#                         )
+#                         if flood_time > 200:
+#                             logger.info(
+#                                 f"Skipping user {user_id} due to excessive FloodWait."
+#                             )
+#                             continue
+#                         await asyncio.sleep(flood_time)
+#                     except Exception as e:
                         
-                        continue
+#                         continue
 
-                await message.reply_text(f"**Broadcasted to {susr} users.**")
+#                 await message.reply_text(f"**Broadcasted to {susr} users.**")
 
-        finally:
-            IS_BROADCASTING = False
+#         finally:
+#             IS_BROADCASTING = False
 
