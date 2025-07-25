32846cf8cde405e6dce83470a17ae0e555a08b86
VootXMusic <updatebot59@gmail.com>
2025-07-25 07:09:00 +0530

    Update callback.py

--- Mickey/modules/callback.py
+++ Mickey/modules/callback.py
@@ -1,5 +1,5 @@
-# Don't remove This Line From Here. Tg: @Dev_Arora_0981 | @DevArora0981
-# Github :- Devarora-0981 | Devarora2604
+# Don't remove This Line From Here. Tg: @I_RAMBHAKT_I | @I_RAMBHAKT_I
+
 
 
 from pyrogram.enums import ChatMemberStatus as CMS
