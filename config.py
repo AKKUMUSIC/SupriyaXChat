3de490df71feac5b4c121e685b571552ce25fc53
AKASH SINGH <singhakku4523@gmail.com>
2026-05-20 20:41:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ BOT_TOKEN = getenv("BOT_TOKEN", None)
 OWNER_ID = int(getenv("OWNER_ID",8556819933))
 MONGO_URL = getenv("MONGO_URL", None)
 SUPPORT_GRP = getenv("SUPPORT_GRP", "AkashUpdate")
-UPDATE_CHNL = getenv("UPDATE_CHNL", "https://t.me/AkashBotNetwork")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "AkashBotNetwork")
 OWNER_USERNAME = getenv("OWNER_USERNAME", "I_RAMBHAKT_I")
 
 
